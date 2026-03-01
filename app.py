7250bb5979543aa23317730bda84d1a1fc25ecb5
Katugwa Nathaniel <nkatugwa@gmail.com>
2026-03-01 07:26:08 +0100

    Fix static routing for vercel

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime, date
 app = Flask(__name__,
             static_folder="static",
             template_folder="templates",
-            static_url_path="/static"
+            static_url_path="/static",
             )
 
 # ============================
